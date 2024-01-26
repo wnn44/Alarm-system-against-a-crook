a25ebc8b55178009bdf3faa2d97e1e532bcc3f3e
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2024-01-26 22:19:13 +0300

    Механика готова

--- Assets/Scripts/AlarmController.cs
+++ Assets/Scripts/AlarmController.cs
@@ -2,27 +2,36 @@ using UnityEngine;
 
 public class AlarmController : MonoBehaviour
 {
+    [SerializeField] private GameObject _thief;
     [SerializeField] private AudioSource _audioSource;
 
-    private void OnEnable()
+    private float _deltaVolume = 0.3f;
+    private float _maxVolume = 1.0f;
+    private float _incrementVolume;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        AlarmSensor.AlarmOn += AlarmStart; 
-        AlarmSensor.AlarmOff += AlarmStop;
+        AlarmStart();
     }
 
-    private void OnDisable()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        AlarmSensor.AlarmOn -= AlarmStart;
-        AlarmSensor.AlarmOff -= AlarmStop;
+        AlarmStop();
     }
 
     private void AlarmStart()
     {
+        _incrementVolume = _deltaVolume;
         _audioSource.Play();
     }
 
-    private void AlarmStop() 
+    private void AlarmStop()
     {
-        _audioSource.Stop();
+        _incrementVolume = -_deltaVolume;
+    }
+
+    private void Update()
+    {
+        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _maxVolume, _incrementVolume * Time.deltaTime);
     }
 }
